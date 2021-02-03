6d0f47a0a08c7398c05e3a662b96a3f5a819d45e
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-02-03 08:45:16 -0300

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
   let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
   conn.sendMessage(m.chat, args.join` `, MessageType.extendedText, { contextInfo: { mentionedJid: users } })
 }
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
+handler.command = /^(anuncio|anunciar)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
