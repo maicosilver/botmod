7ba6c5de7f3b57fa66033a446b8c7a7898ad1548
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-02-03 08:23:40 -0300

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,8 +1,9 @@
-let handler = async (m, { conn, args }) => {
-  let users = args.join` `.split`,`.map(v => v.replace(/[^0-9]/g/, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-  await conn.groupAdd(m.chat, user)
-conn.reply(m.chat, 'Falha ao adicionar contato, talvez porque é privado',m)
+let handler = async (m, { conn, text }) => {
+  let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
+  await conn.groupAdd(m.chat, users)
 }
+handler.help = ['add', '+'].map(v => v + ' nomor,nomor')
+handler.tags = ['admin']
 handler.command = /^(add|\+)$/i
 handler.owner = false
 handler.mods = false
@@ -14,6 +15,7 @@ handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
+handler.limit = true
 
 module.exports = handler
 
